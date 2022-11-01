1d7c37f74695cc53ae3796458066960f1a633441
강수빈 <tnbin98@gmail.com>
2022-11-02 05:12:27 +0900

    test 코드 반영

--- src/main/java/com/example/springboot/dao/HospitalDao.java
+++ src/main/java/com/example/springboot/dao/HospitalDao.java
@@ -2,12 +2,15 @@ package com.example.springboot.dao;
 
 import com.example.springboot.domein.Hospital;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
+
+import java.sql.SQLException;
 
 @Component
 public class HospitalDao {
 
-        private final JdbcTemplate jdbcTemplate;  // Autowired 대신 private final = DI
+        private final JdbcTemplate jdbcTemplate;  // Autowired 할 경우 test 에서 생성자 주입을 해줘야함 대신 private final = DI
 
         public HospitalDao(JdbcTemplate jdbcTemplate) {
             this.jdbcTemplate = jdbcTemplate;
@@ -34,5 +37,46 @@ public class HospitalDao {
                     hospital.getTotalAreaSize()
             );
         }
+
+    public int getCount() {
+        String sql = "select count(*) from nation_wide_hospital";
+        return this.jdbcTemplate.queryForObject(sql, Integer.class);
     }
+
+        public void deleteAll() {
+            this.jdbcTemplate.update("delete from nation_wide_hospitals");
+        }
+
+    RowMapper<Hospital> rowMapper = (rs, rowNum) -> { // RowMapper로 Hospital에 rs 값을 넣어줌
+        Hospital hospital = new Hospital(
+                rs.getInt("id"),
+                rs.getString("open_service_name"),
+                rs.getInt("open_local_government_code"),
+                rs.getString("management_number"),
+                rs.getTimestamp("license_date").toLocalDateTime(),
+                rs.getInt("business_status"),
+                rs.getInt("business_status_code"),
+                rs.getString("phone"),
+                rs.getString("full_address"),
+                rs.getString("road_name_address"),
+                rs.getString("hospital_name"),
+                rs.getString("business_type_name"),
+                rs.getInt("healthcare_provider_count"),
+                rs.getInt("patient_room_count"),
+                rs.getInt("total_number_of_beds"),
+                rs.getFloat("total_area_size")
+        );
+        return hospital;
+    };
+
+    public Hospital findById(int id) throws ClassNotFoundException, SQLException {
+        String sql = "select * from nation_wide_hospital where id = ?";
+        return this.jdbcTemplate.queryForObject(sql, rowMapper, id);
+    }
+
+}
+
+
+
+
 
--- src/test/java/com/example/springboot/parser/HospitalParserTest.java
+++ src/test/java/com/example/springboot/parser/HospitalParserTest.java
@@ -1,40 +1,33 @@
 package com.example.springboot.parser;
 
+import com.example.springboot.dao.HospitalDao;
 import com.example.springboot.domein.Hospital;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class HospitalParserTest {
+    HospitalParser hp = new HospitalParser();
+    String line1 = "\"1\",\"의원\",\"01_01_02_P\",\"3620000\",\"PHMA119993620020041100004\",\"19990612\",\"\",\"01\",\"영업/정상\",\"13\",\"영업중\",\"\",\"\",\"\",\"\",\"062-515-2875\",\"\",\"500881\",\"광주광역시 북구 풍향동 565번지 4호 3층\",\"광주광역시 북구 동문대로 24, 3층 (풍향동)\",\"61205\",\"효치과의원\",\"20211115113642\",\"U\",\"2021-11-17 02:40:00.0\",\"치과의원\",\"192630.735112\",\"185314.617632\",\"치과의원\",\"1\",\"0\",\"0\",\"52.29\",\"401\",\"치과\",\"\",\"\",\"\",\"0\",\"0\",\"\",\"\",\"0\",\"\",";
 
 @Autowired
-ParserFactory parserFactory;
+ReadLineContext<Hospital> hospitalReadLineContext; // 파일 파싱해서 hospital 객체 result list 로 만든 거
 
-    @Test
-    @DisplayName("10만건 이상 데이터가 파싱 되는지")
-    void oneHundreadThousandRows() throws IOException {
-        String filename = "/Users/kangsubin/Desktop/전국병원정보.csv";
-        List<Hospital> hospitalList = new ParserFactory().hospitalReadLineContext().readByLine(filename);
-//        assertTrue(hospitalList.size() > 1000);
-//        assertTrue(hospitalList.size() > 10000);
-        for(int i =0; i< 10; i++){
-            System.out.println(hospitalList.get(i).getHospitalName());
-        }
-        System.out.printf("파싱된 데이터 개수 :%d ", hospitalList.size());
-    }
-
+    @Autowired
+    HospitalDao hospitalDao; // private final 로 jdbc 주입 안했을 경우 생성자 형식으로 작성해야 했음
 
     @Test
     @DisplayName("csv 1줄을 Hospital로 잘 만드는지 Test")
-    void convertToHospital() {
-        String line1 = "\"1\",\"의원\",\"01_01_02_P\",\"3620000\",\"PHMA119993620020041100004\",\"19990612\",\"\",\"01\",\"영업/정상\",\"13\",\"영업중\",\"\",\"\",\"\",\"\",\"062-515-2875\",\"\",\"500881\",\"광주광역시 북구 풍향동 565번지 4호 3층\",\"광주광역시 북구 동문대로 24, 3층 (풍향동)\",\"61205\",\"효치과의원\",\"20211115113642\",\"U\",\"2021-11-17 02:40:00.0\",\"치과의원\",\"192630.735112\",\"185314.617632\",\"치과의원\",\"1\",\"0\",\"0\",\"52.29\",\"401\",\"치과\",\"\",\"\",\"\",\"0\",\"0\",\"\",\"\",\"0\",\"\",";
+    void convertToHospital(String line1) {
 
         HospitalParser hp = new HospitalParser();
         Hospital hospital = hp.parse(line1);
@@ -57,5 +50,36 @@ ParserFactory parserFactory;
         assertEquals(52.29f, hospital.getTotalAreaSize());
     }
 
+    @Test
+    void findById() throws IOException, SQLException, ClassNotFoundException {
+        Hospital hospital = hp.parse(line1);
+        Hospital hospital1 = hospitalDao.findById(1);
+        System.out.println(hospital1.getHospitalName());
+        assertEquals(hospital.getHospitalName(), hospital1.getHospitalName());
+        assertEquals(hospital1.getLicenseDate(), hospital.getLicenseDate());
+    }
+
+    @Test
+    void getCont() throws IOException, SQLException, ClassNotFoundException {
+        int count = hospitalDao.getCount();
+        System.out.println(count);
+        assertEquals(1, count);
+    }
+
+    @Test
+    void add(){
+        Hospital hospital = hp.parse(line1); // 파일 파싱해서 각 칼럼에 값을 hospital에 넣은 상태
+        hospitalDao.add(hospital); // hospital에 담겨 있는 칼럼 값을 insert 쿼리문에 넣음
+    }
+
+    @Test
+    void name() throws IOException {
+        String filename = "/Users/kangsubin/Desktop/전국병원정보.csv";
+        List<Hospital> hospitalList = hospitalReadLineContext.readByLine(filename);
+        assertTrue(hospitalList.size()>1000);
+        assertTrue(hospitalList.size()>10000);
+    }
 }
+
+
 
